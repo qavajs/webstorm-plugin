402531849a2563c883ce9643155ca5e23b03a79c
GitHub Action <action@github.com>
2024-02-07 07:32:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexgalichenko/webstormplugin/MyBundle.kt
+++ src/main/kotlin/com/github/alexgalichenko/webstormplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alexgalichenko.webstormplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alexgalichenko/webstormplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/alexgalichenko/webstormplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.alexgalichenko.webstormplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.alexgalichenko.webstormplugin.MyBundle
+import com.github.alexgalichenko.webstormplugin.services.MyProjectService
 import javax.swing.JButton
 
 
